ae30fde798f6f981ba58702fcf1bd3bfa6170525
Zaighum <zaighum47@Zaighums-MacBook-Air.local>
2013-07-23 16:41:25 -0400

    Fixed Gamma Calculation

--- BasicEuropeanCallPutPricer.R
+++ BasicEuropeanCallPutPricer.R
@@ -34,12 +34,12 @@ BSPricer <- function(CallOrPut = "CALL", SpotPrice = 20, Strike = 19, Vol = 0.25
   #PUT
   #########
   if(CallOrPut == "PUT"){
-  return = "here"
+  
   Price = Strike*exp(-RiskFree*T)*pnorm(-d2) - SpotPrice*pnorm(-d1)
+  
   Delta = pnorm(d1) - 1
   
   Gamma = (exp(-d1^2 /2))/(SpotPrice*Vol*sqrt(2*pi*T)) ##Gamma of Call and Option are equal
-  
   
   return(list(Price = Price, Delta = Delta, Gamma = Gamma)) 
   }
